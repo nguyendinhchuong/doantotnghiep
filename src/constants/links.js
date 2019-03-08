a0f5c7d9a2714895c97e3a22172038b5e99971f7
Minh Nhat <mocking.key118@gmail.com>
2019-03-08 16:30:05 +0700

    change links of API

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,23 +1,28 @@
 export const LINK = "http://localhost:3001";
 
 // faculties links
-export const LOAD_FACULTIES = `${LINK}/faculties/`;
+export const LOAD_FACULTIES = `${LINK}/getFaculties/`;
 
 // programs links
-export const LOAD_PROGRAMS = `${LINK}/programs/`;
+export const LOAD_PROGRAMS = `${LINK}/getPrograms/`;
 
 // outcomeStandards links
-export const LOAD_OUTCOMESTANDARDS = `${LINK}/outcomeStandards/`;
-
-export const DELETE_OUTCOMESTANDARD = `${LINK}/outcomeStandard/`;
-export const ADD_OUTCOMESTANDARD = `${LINK}/outcomeStandard/`;
+export const LOAD_OUTCOMESTANDARDS = `${LINK}/getOutcomeStandards/`;
+export const LOAD_OUTCOMESTANDARD = `${LINK}/getOutcomeStandard/`;
+export const DELETE_OUTCOMESTANDARD = `${LINK}/deleteOutcomeStandard/`;
+export const ADD_OUTCOMESTANDARD = `${LINK}/addOutcomeStandard/`;
 
 // detailOutcomeStandard links
-export const SAVE_DETAIL_OUTCOMESTANDARD = `${LINK}/detailOutcomeStandard/`;
-export const ADD_DETAIL_OUTCOMESTANDARD = `${LINK}/detailOutcomeStandard/`;
-// need to change
-export const LOAD_DETAIL_OUTCOMESTANDARD = `${LINK}/listOutcomeStandard/`;
+export const LOAD_DETAIL_OUTCOMESTANDARD = `${LINK}/getDetailOutcomeStandard/`;
+export const SAVE_DETAIL_OUTCOMESTANDARD = `${LINK}/saveDetailOutcomeStandard/`;
+export const ADD_DETAIL_OUTCOMESTANDARD = `${LINK}/addDetailOutcomeStandard/`;
 
-// infoOutcomeStandard links
-export const LOAD_INFO_OUTCOMESTANDARD = `${LINK}/infoOutcomeStandard/`;
-export const CREATE_INFO_OUTCOMESTANDARD = `${LINK}/infoOutcomeStandard/`;
+// revisions links
+export const LOAD_REVISIONS = `${LINK}/getRevisions/`;
+export const DELETE_REVISION = `${LINK}/deleteRevision/`;
+export const ADD_REVISION = `${LINK}/addRevision/`;
+
+// detailOutcomeStandard links
+export const LOAD_DETAIL_REVISION = `${LINK}/getDetailRevision/`;
+export const SAVE_DETAIL_REVISION = `${LINK}/saveDetailRevision/`;
+export const ADD_DETAIL_REVISION = `${LINK}/addDetailRevision/`;
