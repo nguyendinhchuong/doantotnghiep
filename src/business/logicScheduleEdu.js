07bed5284afacfab280c2f9ff1ea8154ddfeb2ba
Minh Nhat <mocking.key118@gmail.com>
2019-05-10 20:38:21 +0700

    code 8: ScheduleEducation

--- src/business/logicScheduleEdu.js
+++ src/business/logicScheduleEdu.js
@@ -3,100 +3,14 @@ import { Column } from "primereact/column";
 import { ColumnGroup } from "primereact/columngroup";
 import { Row } from "primereact/row";
 
-import * as common from "./commonEducation";
-
-const NAME_SEMESTER = "HỌC KỲ ";
-const LEVEL = 8;
-
-// add
-export const addRoot = (nodes, semester) => {
-  const key = `${LEVEL}.${nodes.length + 1}`;
-  const node = {
-    key: key,
-    data: {
-      name: semester,
-      displayName: `${key}. ${NAME_SEMESTER + semester}`
-    },
-    children: []
-  };
-  let results = [...nodes, node];
-  return results;
-};
-
-// Delete Node
-export const deleteNode = (nodes, node) => {
-  debugger;
-  let root = [...nodes];
-  // index of root
-  const idRoot = common.indexRoot(node.key);
-  const index = common.indexNode(node.key);
-
-  // ROOT
-  if (common.getRank(node.key) === 2) {
-    root.splice(index, 1);
-    root = refreshTreeNodes(root, node.key, idRoot - 1);
-    return root;
-  }
-  let parentKey = common.parentKey(node.key);
-  let rootKey = common.keyRoot(node.key);
-  // case root = 7.1.... => 1.1...
-  if (nodes[0].key[0] === "8") {
-    const firstDot = parentKey.indexOf(".");
-    parentKey = parentKey.slice(firstDot + 1, parentKey.length);
-  }
-  const parentNode = common.findNodeByKey(root, parentKey);
-  parentNode.children.splice(index, 1);
-  root = common.updateNode(root, parentNode);
-  root = refreshTreeNodes(root, rootKey, idRoot - 1);
-  return root;
-};
-
-const updateSubNode = (iParent, node) => {
-  if (node.children) {
-    const length = node.children.length;
-    for (let i = 0; i < length; i++) {
-      node.children[i].key = `${iParent}.${i + 1}`;
-      if (!node.children[i].data.isTable) {
-        node.children[i].data.displayName = `${
-          node.children[i].key
-        }. ${NAME_SEMESTER + node.children[i].data.name}`;
-      }
-      if (node.children[i].data.isTable) {
-        node.children[i].data.subjects = common.updateKeyParentOfSubjects(
-          node.children[i].data.subjects,
-          node.children[i].key
-        );
-      }
-      if (node.children[i].children)
-        updateSubNode(node.children[i].key, node.children[i]);
-    }
-  }
-};
-
-export const refreshTreeNodes = (nodes, key, indexRefresh) => {
-  const data = [...nodes];
-  const length = data.length;
-
-  for (let i = indexRefresh; i < length; i++) {
-    const keyIncrease = common.increaseKey(key, i + 1);
-    data[i].key = keyIncrease;
-    data[i].data.displayName = `${keyIncrease}. ${NAME_SEMESTER +
-      data[i].data.name}`;
-
-    updateSubNode(data[i].key, data[i]);
-  }
-  return data;
-};
-
-//
 export const headerGroup = (
   <ColumnGroup>
     <Row>
-      <Column header="Loại Học Phần" rowSpan={2} />
       <Column header="STT" rowSpan={2} />
       <Column header="Mã Học Phần" rowSpan={2} />
       <Column header="Tên Học Phần" rowSpan={2} />
-      <Column header="Số Tín Chỉ" rowSpan={2} />
+      <Column header="Loại HP" rowSpan={2} />
+      <Column header="Số TC" rowSpan={2} />
       <Column header="Số Tiết" colSpan={3} />
       <Column header="Ghi Chú" rowSpan={2} />
       <Column rowSpan={2} />
@@ -109,23 +23,6 @@ export const headerGroup = (
   </ColumnGroup>
 );
 
-// subject
-const checkExistsSubject = (subjects, subject) => {
-  for (let i = 0; i < subjects.length; i++) {
-    if (subjects[i].SubjectName === subject.SubjectName) {
-      return true;
-    }
-  }
-  return false;
-};
-
-export const addSubjectInOnchange = (subjects, subject) => {
-  if (checkExistsSubject(subjects, subject)) {
-    return subjects;
-  }
-  return [...subjects, subject];
-};
-
 export const filterSubjects = (e, subjects) => {
   const re = new RegExp(e.query.toLowerCase());
   const results = subjects
@@ -136,55 +33,18 @@ export const filterSubjects = (e, subjects) => {
   return results;
 };
 
-export const deteleSubject = (subjects, subject) => {
-  const indexSubject = subjects.indexOf(subject);
-  return subjects.filter((subject, index) => {
-    if (indexSubject !== index) {
-      return subject;
-    }
-  });
-};
-
-export const sortSubject = data => {
-  return data.sort((a, b) => {
-    const option1 = a.option;
-    const option2 = b.option;
-    if (option1 === option2) {
-      const code1 = a.SubjectCode;
-      const code2 = b.SubjectCode;
-      return code1.localeCompare(code2);
-    }
-    return option1.localeCompare(option2);
-  });
-};
-
-export const toltalRequiredCredits = subjects => {
-  return subjects.reduce((acc, cur) => {
-    if (cur.option === "BB" && cur.Credit) {
-      return acc + Number(cur.Credit);
-    }
-    return acc;
-  }, 0);
-};
-
-export const indexSubjects = data => {
-  const results = data.reduce((acc, cur, index) => {
-    cur.index = index + 1;
-    return acc.concat(cur);
-  }, []);
-  return results;
-};
-
-export const deleteSubjectTable = (nodes, subject) => {
-  let root = [...nodes];
-  let keyParent = subject.parentKey;
-  // case root = 7.1.... => 1.1...
-  if (nodes[0].key[0] === "8") {
-    const firstDot = keyParent.indexOf(".");
-    keyParent = keyParent.slice(firstDot + 1, keyParent.length);
+export const addSubjectInOnchange = (subjects, subject) => {
+  if (checkExistsSubject(subjects, subject)) {
+    return subjects;
   }
-  const parentNode = common.findNodeByKey(root, keyParent);
-  parentNode.data.subjects = deteleSubject(parentNode.data.subjects, subject);
-  root = common.updateNode(root, parentNode);
-  return root;
+  return [...subjects, subject];
+};
+
+const checkExistsSubject = (subjects, subject) => {
+  for (let i = 0; i < subjects.length; i++) {
+    if (subjects[i].SubjectName === subject.SubjectName) {
+      return true;
+    }
+  }
+  return false;
 };
--- src/components/detailEducationProgram/ScheduleEducationCom.js
+++ src/components/detailEducationProgram/ScheduleEducationCom.js
@@ -1,96 +1,70 @@
 import React from "react";
-import { TreeTable } from "primereact/treetable";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Row, Col, Button } from "shards-react";
 import { Dialog } from "primereact/dialog";
-import { Spinner } from "primereact/spinner";
 import { AutoComplete } from "primereact/autocomplete";
-import { InputTextarea } from "primereact/inputtextarea";
 import { OrderList } from "primereact/orderlist";
-import { Checkbox } from "primereact/checkbox";
+import { Spinner } from "primereact/spinner";
 
 import * as logic from "../../business/logicScheduleEdu";
-import * as common from "../../business/commonEducation";
-
-import TableScheduleSubjectCom from "./TableScheduleSubjectCom";
 
 export default class ScheduleEducationCom extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nodes: [],
-      node: {},
-      semester: 1,
-      nodeHover: "",
-      isRequired: true,
-      filterSubjects: [],
-      optionSubjects: [],
-      listSubjects: [], // add into table
-      isDialogRoot: false,
       isDialogTable: false,
-      isDialogSubjectsTable: false
+      semester: 1,
+      filterSubjects: [],
+      listSubjects: [],
+      optionSubjects: [],
+      semesters: [
+        {
+          num: 1,
+          subjects: [
+            {
+              Id: 2,
+              SubjectCode: "BAA00002",
+              SubjectName: "Đường lối cách mạng của ĐCSVN",
+              Credit: 3,
+              TheoryPeriod: 45,
+              PracticePeriod: 0,
+              ExercisePeriod: 0
+            }
+          ]
+        },
+        {
+          num: 2
+        },
+        {
+          num: 3
+        }
+      ],
+      tmpSubject: []
     };
   }
-  // add
-  handleAddRoot = () => {
-    const data = logic.addRoot(this.state.nodes, this.state.semester);
-    this.setState({ nodes: data, semester: this.state.semester + 1 });
-    this.onHideDialogRoot();
-  };
 
-  handleAddChild = () => {
+  onHideAddSemester = () => {
     this.setState({
-      nodes: this.addChildTable(this.state.nodes, this.state.node)
+      isDialogTable: false
     });
-    this.onHideDialogTable();
   };
 
-  addChildTable = (nodes, nodeParent) => {
-    let data = [...nodes];
-    const length = nodeParent.children.length;
-    const key = `${nodeParent.key}.${length + 1}`;
-    let node = {
-      key: key,
-      data: {
-        isTable: true,
-        subjects: [],
-        displayName: ""
-      },
-      children: []
-    };
-    node = this.convertNodeToDataTable(node);
-    nodeParent.children.push(node);
-    data = common.updateNode(data, nodeParent);
-    return data;
+  OnOpenAddSemester = () => {
+    this.setState({
+      isDialogTable: true
+    });
   };
 
-  convertNodeToDataTable = node => {
-    if (!node.data.isTable) {
-      return node;
-    }
-    node.data.displayName = (
-      <TableScheduleSubjectCom
-        subjects={node.data.subjects}
-        deleteSubject={this.deleteSubjectOnTable}
-        //sum={node.data.totalCredits}
-      />
-    );
-    return node;
+  filterSubjects = e => {
+    this.setState({
+      filterSubjects: logic.filterSubjects(e, this.props.subjects)
+    });
   };
-
-  // Delete
-  deleteNode = node => {
-    const root = logic.deleteNode(this.state.nodes, node);
-    this.setState({ nodes: root });
-  };
-
-  // onchange
 
   onChangeListSubjects = e => {
     if (typeof e.value === "object") {
       const subject = e.value;
-      subject.option = this.state.isRequired ? "BB" : "TC";
       const subjects = logic.addSubjectInOnchange(
         this.state.listSubjects,
         subject
@@ -100,137 +74,38 @@ export default class ScheduleEducationCom extends React.Component {
     this.setState({ optionSubjects: e.value });
   };
 
-  filterSubjects = e => {
-    this.setState({
-      filterSubjects: logic.filterSubjects(e, this.props.subjects)
-    });
-  };
-
-  requiredSubjects = () => {
-    return this.state.listSubjects.filter(subject => {
-      if (subject.option === "BB") {
-        return subject;
-      }
-    });
-  };
-
-  notRequiredSubjects = () => {
-    return this.state.listSubjects.filter(subject => {
-      if (subject.option === "TC") {
-        return subject;
-      }
-    });
-  };
-
-  // show/hidden Dialong
-  isShowDialogRoot = () => {
-    this.setState({
-      isDialogRoot: true
-    });
-  };
-
-  isShowDialogTable = node => {
-    this.setState({
-      isDialogTable: true,
-      node: node
-    });
-  };
-
-  isShowDialogSubjectsTable = node => {
-    this.setState({
-      isDialogSubjectsTable: true,
-      node: node,
-      listSubjects: []
-    });
-  };
-
-  onHideDialogRoot = () => {
-    this.setState({ isDialogRoot: false });
-  };
-
-  onHideDialogTable = () => {
+  addSemester = () => {
     this.setState({
       isDialogTable: false
     });
   };
 
-  onHideDialogSubjectsTable = () => {
-    this.setState({
-      isDialogSubjectsTable: false
-    });
-  };
-  // subjects
-
-  deleteSubject = subject => {
-    this.setState({
-      listSubjects: logic.deteleSubject(this.state.listSubjects, subject)
-    });
-  };
-
-  // table subject
-
-  loadSubNode = node => {
-    if (node.children) {
-      const length = node.children.length;
-      for (let i = 0; i < length; i++) {
-        if (node.children[i].data.isTable) {
-          node.children[i] = this.convertNodeToDataTable(node.children[i]);
-        }
-        if (node.children[i].children) {
-          this.loadSubNode(node.children[i]);
-        }
-      }
-    }
-  };
-
-  loadTreeNodes = nodes => {
-    const root = [...nodes];
-    const length = root.length;
-    for (let i = 0; i < length; i++) {
-      this.loadSubNode(root[i]);
-    }
-    return root;
-  };
-
-  addRowTable = () => {
-    const data = this.addRowTableLogic(
-      this.state.nodes,
-      this.state.node,
-      this.state.listSubjects
+  rowExpansionTemplate = data => {
+    return (
+      <DataTable headerColumnGroup={logic.headerGroup} value={data.subjects}>
+        <Column field="Id" />
+        <Column field="SubjectCode" />
+        <Column field="SubjectName" />
+        <Column field="option" />
+        <Column field="Credit" />
+        <Column field="TheoryPeriod" />
+        <Column field="PracticePeriod" />
+        <Column field="ExercisePeriod" />
+        <Column field="note" />
+      </DataTable>
     );
-    this.setState({ nodes: data });
-    this.onHideDialogSubjectsTable();
   };
 
-  addRowTableLogic = (nodes, node, subjectsAdd) => {
-    if (!node.data.isTable) {
-      return nodes;
-    }
-    let child = { ...node };
-    let root = [...nodes];
-    let subjectsTable = child.data.subjects;
-    subjectsAdd.forEach(subject => {
-      subject.parentKey = child.key;
-      subjectsTable = logic.addSubjectInOnchange(subjectsTable, subject);
-    });
-    child.data.subjects = subjectsTable;
-    child.data.totalCredits =
-      logic.toltalRequiredCredits(child.data.subjects) +
-      this.state.optionalCredit;
-    child.data.subjects = logic.sortSubject(child.data.subjects);
-    child.data.subjects = logic.indexSubjects(child.data.subjects);
-    child = this.convertNodeToDataTable(child);
-    root = common.updateNode(root, child);
-    return root;
-  };
-
-  deleteSubjectOnTable = rowData => {
-    let root = logic.deleteSubjectTable(this.state.nodes, rowData);
-    root = this.loadTreeNodes(root);
-    this.setState({ nodes: root });
-  };
-
-  // tempplate table
+  footerDialogTable = (
+    <div>
+      <Button onClick={this.addSemester} theme="success">
+        Thêm
+      </Button>
+      <Button onClick={this.onHideAddSemester} theme="secondary">
+        Hủy
+      </Button>
+    </div>
+  );
 
   subjectTemplate = subject => {
     return (
@@ -260,231 +135,79 @@ export default class ScheduleEducationCom extends React.Component {
     );
   };
 
-  // Template
-  actionTemplate(node, column) {
-    return (
-      <div>
-        {!node.data.isTable ? (
-          <Button
-            onClick={() => this.isShowDialogTable(node)}
-            theme="success"
-            style={{ marginRight: ".3em", padding: "8px" }}
-            title={`Thêm bảng môn học`}
-          >
-            <i className="material-icons">add</i>
-          </Button>
-        ) : (
-          <Button
-            onClick={() => this.isShowDialogSubjectsTable(node)}
-            theme="success"
-            style={{ marginRight: ".3em", padding: "8px" }}
-            title={`Thêm môn học`}
-          >
-            <i className="material-icons">playlist_add</i>
-          </Button>
-        )}
-        <Button
-          onClick={() => this.deleteNode(node)}
-          //onMouseOver={() => this.mouseOver(node)}
-          theme="secondary"
-          style={{ marginRight: ".3em", padding: "8px" }}
-          title={`Xóa cấp ${this.state.nodeHover}`}
-        >
-          <i className="material-icons">delete_sweep</i>
-        </Button>
-      </div>
-    );
-  }
-
-  footerRoot = (
-    <div>
-      <Button onClick={this.handleAddRoot} theme="success">
-        Thêm
-      </Button>
-      <Button onClick={this.onHideDialogRoot} theme="secondary">
-        Hủy
-      </Button>
-    </div>
-  );
-
-  footerChildTable = (
-    <div>
-      <Button onClick={this.handleAddChild} theme="success">
-        Thêm
-      </Button>
-      <Button onClick={this.onHideDialogTable} theme="secondary">
-        Hủy
-      </Button>
-    </div>
-  );
-
-  footerDialogSubjectsTable = (
-    <div>
-      <Button onClick={this.addRowTable} theme="success">
-        Thêm
-      </Button>
-      <Button onClick={this.onHideDialogTable} theme="secondary">
-        Hủy
-      </Button>
-    </div>
-  );
-
   render() {
     return (
-      <div>
-        <TreeTable value={this.state.nodes}>
-          <Column field="displayName" header="Tên dòng" expander />
+      <div className="content-section implementation">
+        <DataTable
+          value={this.state.semesters}
+          expandedRows={this.state.expandedRows}
+          onRowToggle={e => this.setState({ expandedRows: e.data })}
+          rowExpansionTemplate={this.rowExpansionTemplate}
+        >
           <Column
+            field="num"
+            style={{ width: "5em" }}
             header={
-              <Button onClick={() => this.isShowDialogRoot()} theme="success">
-                <i className="material-icons">add</i> Thêm Học kỳ
+              <Button
+                title="Thêm học kì"
+                onClick={this.OnOpenAddSemester}
+                theme="success"
+              >
+                <i className="material-icons">add</i>
               </Button>
             }
-            body={(node, column) => this.actionTemplate(node, column)}
-            style={{ textAlign: "center", width: "12em" }}
+            expander={true}
           />
-        </TreeTable>
-
-        {/* Dialog Root */}
+          <Column field="num" header="HỌC KÌ" />
+        </DataTable>
         <Dialog
-          header="Thêm Học kỳ"
-          visible={this.state.isDialogRoot}
-          onHide={() => this.onHideDialogRoot()}
+          header="Thêm Học Kì"
+          visible={this.state.isDialogTable}
+          onHide={this.onHideAddSemester}
           style={{ width: "50vw" }}
-          footer={this.footerRoot}
+          footer={this.footerDialogTable}
         >
           <Row>
-            <Col lg="2" md="2" sm="4">
-              <label>Học kỳ: </label>
+            <Col lg="3" md="3" sm="3" offset="3">
+              <label>Học kì:</label>
             </Col>
-            <Col lg="2" md="2" sm="4">
+            <Col lg="1" md="1" sm="2" offset="3">
               <Spinner
                 value={this.state.semester}
                 onChange={e => {
-                  if (e.value > 0) this.setState({ semester: e.value });
+                  if (e.value > 0 && e.value < 13)
+                    this.setState({ semester: e.value });
                 }}
               />
             </Col>
           </Row>
-        </Dialog>
-        {/*Dialog Table */}
-        <Dialog
-          header="Cấu trúc"
-          visible={this.state.isDialogTable}
-          onHide={() => this.onHideDialogTable()}
-          style={{ width: "50vw" }}
-          footer={this.footerChildTable}
-        >
-          <div>
-            <DataTable headerColumnGroup={logic.headerGroup}>
-              <Column header="STT" />
-              <Column header="Loại Học Phần" />
-              <Column header="Mã Môn Học" />
-              <Column header="Tên Môn Học" />
-              <Column header="Số Tín Chỉ" />
-              <Column header="Lý Thuyết" />
-              <Column header="Thực Hành" />
-              <Column header="Bài Tập" />
-              <Column header="Description" />
-            </DataTable>
-          </div>
-        </Dialog>
-        {/* Dialog of dataTable */}
-        <Dialog
-          header="Thêm Nội Dung Môn Học"
-          visible={this.state.isDialogSubjectsTable}
-          onHide={() => this.onHideDialogSubjectsTable()}
-          style={{ width: "50vw" }}
-          footer={this.footerDialogSubjectsTable}
-        >
+          <br />
           <Row>
-            <Col lg="2" md="2" sm="2">
-              <span>Môn Học:</span>
+            <Col lg="3" md="3" sm="3">
+              <span>Môn Học thêm vào:</span>
             </Col>
-            <Col lg="6" md="6" sm="12">
+            <Col lg="9" md="9" sm="9">
               <AutoComplete
                 field="SubjectName"
                 value={this.state.optionSubjects}
                 dropdown={true}
                 onChange={e => this.onChangeListSubjects(e)}
                 size={40}
-                placeholder="Môn học"
+                placeholder="Toán rời rạc"
                 minLength={1}
                 suggestions={this.state.filterSubjects}
                 completeMethod={e => this.filterSubjects(e)}
               />
             </Col>
           </Row>
-          <div hidden={!this.state.isRequired}>
-            <Row style={{ marginTop: "15px", marginBottom: "15px" }}>
-              <Col lg="2" md="2" sm="2">
-                <span>Môn Học BB:</span>
-              </Col>
-              <Col lg="10" md="10" sm="12">
-                <OrderList
-                  value={this.state.listSubjects.filter(subject => {
-                    if (subject.option === "BB") {
-                      return subject;
-                    }
-                  })}
-                  responsive={true}
-                  itemTemplate={this.subjectTemplate}
-                />
-              </Col>
-            </Row>
-          </div>
-          <div hidden={this.state.isRequired}>
-            <Row style={{ marginTop: "15px", marginBottom: "15px" }}>
-              <Col lg="2" md="2" sm="2">
-                <span>Môn Học TC:</span>
-              </Col>
-              <Col lg="10" md="10" sm="12">
-                <OrderList
-                  value={this.state.listSubjects.filter(subject => {
-                    if (subject.option === "TC") {
-                      return subject;
-                    }
-                  })}
-                  responsive={true}
-                  itemTemplate={this.subjectTemplate}
-                />
-              </Col>
-            </Row>
-          </div>
-
-          <Row style={{ marginTop: "15px", marginBottom: "15px" }}>
-            <Col lg="2" md="2" sm="2">
-              <label>Loại Học Phần:</label>
-            </Col>
-            <Col lg="3" md="3" sm="3">
-              <Checkbox
-                checked={this.state.isRequired}
-                onChange={e => this.setState({ isRequired: true })}
-              />
-              <label htmlFor="cb2" className="p-checkbox-label">
-                Bắt Buộc
-              </label>
-            </Col>
-            <Col lg="2" md="2" sm="2">
-              <Checkbox
-                checked={!this.state.isRequired}
-                onChange={e => this.setState({ isRequired: false })}
-              />
-              <label htmlFor="cb2" className="p-checkbox-label">
-                Tự Chọn
-              </label>
-            </Col>
-          </Row>
+          <hr />
           <Row>
-            <Col lg="2" md="2" sm="6">
-              <label>Ghi chú: </label>
-            </Col>
-            <Col lg="10" md="10" sm="12">
-              <InputTextarea
-                rows={1}
-                cols={30}
-                value={this.state.note}
-                onChange={e => this.setState({ note: e.target.value })}
+            <Col lg="12" md="12" sm="12">
+              <OrderList
+                header="Danh Sách Môn Học:"
+                responsive={true}
+                value={this.state.listSubjects}
+                itemTemplate={this.subjectTemplate}
               />
             </Col>
           </Row>
